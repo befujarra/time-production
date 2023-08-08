ec6f9b84fb5194ef0e11749931034e1f12e91878
Vinicius Fujarra <107464091+befujarra@users.noreply.github.com>
2023-08-07 21:57:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ document.getElementById("calcular").addEventListener("click", function () {
     document.getElementById("velocidadeMaq").value
   );
 
-  const contaMin = qtdBisnagas / velocidadeMaq;
-  const contaHora = contaMin / 60;
-  const resultadoFinal = contaHora.toFixed(1);
+  const minutosTotal = qtdBisnagas / velocidadeMaq;
+  const horasProducao = Math.floor(minutosTotal / 60);
+  const minutosRestantes = Math.round(minutosTotal % 60);
 
-  document.getElementById("resultado").innerText =
-    "Falta " + resultadoFinal + " hora(s) de produção";
+  const now = new Date();
+  const horaAtual = now.getHours();
+  const minutosAtual = now.getMinutes();
+
+  const minutosTermino = minutosAtual + minutosRestantes;
+  let horaTermino = horaAtual + horasProducao + Math.floor(minutosTermino / 60);
+
+  if (horaTermino >= 24) {
+    horaTermino %= 24;
+  }
+
+  if (horaTermino === 0) {
+    horaTermino = 12; // Meia-noite
+  } else {
+    horaTermino = horaTermino % 12 || 12;
+  }
+
+  const ampm = horaAtual < 12 ? "AM" : "PM";
+
+  document.getElementById(
+    "resultado"
+  ).innerText = `A produção irá terminar às ${horaTermino}:${
+    minutosTermino < 10 ? "0" : ""
+  }${minutosTermino % 60} ${ampm}`;
 });
